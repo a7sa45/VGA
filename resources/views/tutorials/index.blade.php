60fcc3b6b75dbb3880a622ab1e8addc6cb358a57
ABDULHADI AL-BO-SNINAH <36082752+a7sa45@users.noreply.github.com>
2023-10-18 14:57:58 +0300

    add font awosem & add the add button

--- resources/views/tutorials/index.blade.php
+++ resources/views/tutorials/index.blade.php
@@ -17,7 +17,10 @@
             <div class="row row-cols-1 row-cols-md-3 g-4">
                 <div class="col">
                   <div class="card h-100">
-                    <img src="https://dytvr9ot2sszz.cloudfront.net/wp-content/uploads/2019/05/1200x628_logstash-tutorial-min.jpg" class="card-img-top" alt="...">
+                    <div class="card-header">
+                        <small class="text-body-secondary">from username</small>
+                    </div>
+                    <img src="{{ asset('images/tutorial.jpeg') }}" class="" alt="...">
                     <div class="card-body">
                       <h5 class="card-title">Tutorail title</h5>
                       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -29,7 +32,10 @@
                 </div>
                 <div class="col">
                   <div class="card h-100">
-                    <img src="https://dytvr9ot2sszz.cloudfront.net/wp-content/uploads/2019/05/1200x628_logstash-tutorial-min.jpg" class="card-img-top" alt="...">
+                    <div class="card-header">
+                        <small class="text-body-secondary">from username</small>
+                    </div>
+                    <img src="{{ asset('images/tutorial.jpeg') }}" class="" alt="...">
                     <div class="card-body">
                       <h5 class="card-title">Tutorail title</h5>
                       <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
@@ -41,7 +47,10 @@
                 </div>
                 <div class="col">
                   <div class="card h-100">
-                    <img src="https://dytvr9ot2sszz.cloudfront.net/wp-content/uploads/2019/05/1200x628_logstash-tutorial-min.jpg" class="card-img-top" alt="...">
+                    <div class="card-header">
+                        <small class="text-body-secondary">from username</small>
+                    </div>
+                    <img src="{{ asset('images/tutorial.jpeg') }}" class="" alt="...">
                     <div class="card-body">
                       <h5 class="card-title">Tutorail title</h5>
                       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
@@ -55,4 +64,16 @@
         </div>
     </div>
 </div>
+
+
+
+@auth
+@if (Auth::user()->role == "teacher")
+<div class="position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
+    <a class="btn bg-info btn-bd-primary py-2  d-flex align-items-center" type="button" href="tutorials/create">
+        <i class="fa-solid fa-plus"></i>
+    </a>
+</div>
+@endif
+@endauth
 @endsection
